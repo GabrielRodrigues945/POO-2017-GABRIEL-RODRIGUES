5e02f44304253eb143138e1a1550e33715140040
GabrielRodrigues945 <you@example.comgrodrigues945@gmail.com>
2017-11-09 15:22:45 -0300

    Atividade B

---
+++ ATIVIDADE B/Questao2/src/questao2/Questao2.java
@@ -0,0 +1,15 @@
+package questao2;
+public class Questao2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int number = 1;
+        if (number <= 100); {
+        sum = number + sum;
+        number = 1 + number;
+       
+        
+    }  
+                
+    }
+    
+}
---
+++ ATIVIDADE B/QuestãoB2/src/questãob2/QuestãoB2.java
@@ -0,0 +1,18 @@
+package questãob2;
+
+public class QuestãoB2 {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int number = 1;
+        if (number <= 1000000); {
+        sum += number;
+        number++;
+            
+       
+        
+    }  
+                
+    }
+    
+}
